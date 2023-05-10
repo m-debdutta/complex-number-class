41cd78a7fec472463acd1b3d5252fddd04c36865
Debdutta <m-debdutta@users.noreply.github.com>
2023-05-10 17:00:09 +0530

    fixed test messages

--- test/realNumber-test.js
+++ test/realNumber-test.js
@@ -1,115 +1,114 @@
-const {describe, it} = require('node:test'); 
-const {ok, strictEqual} = require('assert'); 
-const {RealNumber} = require('../src/realNumber.js'); 
+const { describe, it } = require("node:test");
+const { ok, strictEqual } = require("assert");
+const { RealNumber } = require("../src/realNumber.js");
 
-describe('Testing toString() ', function() {
-  it('should display the real number', function() {
-    const five = new RealNumber(5); 
+describe("Testing toString() ", function () {
+  it("should display the real number", function () {
+    const five = new RealNumber(5);
 
-    const actual = five.toString(); 
-    const expected = '5'; 
+    const actual = five.toString();
+    const expected = "5";
 
-    strictEqual(actual, expected); 
-  }); 
+    strictEqual(actual, expected);
+  });
 
-  it('should display a negative real number', function() {
+  it("should display a negative real number", function () {
     const minusFive = new RealNumber(-5);
 
-    const actual = minusFive.toString(); 
-    const expected = '-5'; 
+    const actual = minusFive.toString();
+    const expected = "-5";
 
-    strictEqual(actual, expected); 
-  }); 
+    strictEqual(actual, expected);
+  });
 });
 
-describe('Testing areEqual() ', function() {
- 
-  it('should state the given numbers are equal', function() {
+describe("Testing areEqual() ", function () {
+  it("should state the given numbers are equal", function () {
     const three = new RealNumber(3);
     const result = three.areEqual(three);
 
     strictEqual(result, true);
-  }); 
+  });
 
-  it('should state the given numbers are not equal', function() {
+  it("should state the given numbers are not equal", function () {
     const three = new RealNumber(3);
     const four = new RealNumber(4);
 
-    const result  = three.areEqual(four);
+    const result = three.areEqual(four);
     strictEqual(result, false);
   });
 });
 
-describe('Testing add() ', function() {
-  it('should display the addition of two real number', function() {
-    const four = new RealNumber(4); 
+describe("Testing add() ", function () {
+  it("should add two real number", function () {
+    const four = new RealNumber(4);
     const five = new RealNumber(3);
 
-    const actual = four.add(five); 
-    const expected = new RealNumber(7); 
+    const actual = four.add(five);
+    const expected = new RealNumber(7);
 
     const result = actual.areEqual(expected);
-    ok(result); 
+    ok(result);
   });
-  
-  it('should display the addition of two real number of opposite sign', function() {
-    const four = new RealNumber(4); 
-    const minusThree = new RealNumber(-3); 
+
+  it("should add two real number of opposite sign", function () {
+    const four = new RealNumber(4);
+    const minusThree = new RealNumber(-3);
 
     const actual = four.add(minusThree);
-    const expected = new RealNumber(1); 
-    
+    const expected = new RealNumber(1);
+
     const result = actual.areEqual(expected);
-    ok(result); 
+    ok(result);
   });
 });
 
-describe('Testing multiply()', function() {
-  it('should display the multiplication of two real numbers', function() {
-    const four = new RealNumber(4); 
-    const three = new RealNumber(3); 
+describe("Testing multiply()", function () {
+  it("should multiply two real numbers", function () {
+    const four = new RealNumber(4);
+    const three = new RealNumber(3);
 
     const expected = new RealNumber(12);
     const actual = four.multiply(three);
-    
+
     const result = actual.areEqual(expected);
-    ok(result); 
+    ok(result);
   });
 
-  it('should display the multiplication of two real numbers of opposite sign', function() {
-    const minusFour = new RealNumber(-4); 
-    const three = new RealNumber(3); 
+  it("should multiply two real numbers of opposite sign", function () {
+    const minusFour = new RealNumber(-4);
+    const three = new RealNumber(3);
 
     const expected = new RealNumber(-12);
-    const actual = minusFour.multiply(three); 
+    const actual = minusFour.multiply(three);
 
     const result = actual.areEqual(expected);
-    ok(result); 
+    ok(result);
   });
 });
 
-  describe('Testing isNegative() ', function() {
-    it('minus-four is a negative', function() {
-      const minusFour = new RealNumber(-4);    
-     
-      const actual = minusFour.isNegative();
+describe("Testing isNegative() ", function () {
+  it("minus-four is a negative", function () {
+    const minusFour = new RealNumber(-4);
 
-      strictEqual(actual, true);
-    });
+    const actual = minusFour.isNegative();
 
-    it('four is not a negative number', function() {
-      const four = new RealNumber(4);    
-     
-      const actual = four.isNegative();
-
-      strictEqual(actual, false);
-    });
-
-    it('zero is not a negative number', function() {
-      const zero = new RealNumber(0);    
-     
-      const actual = zero.isNegative();
-
-      strictEqual(actual, false);
-    });
+    strictEqual(actual, true);
   });
+
+  it("four is not a negative number", function () {
+    const four = new RealNumber(4);
+
+    const actual = four.isNegative();
+
+    strictEqual(actual, false);
+  });
+
+  it("zero is not a negative number", function () {
+    const zero = new RealNumber(0);
+
+    const actual = zero.isNegative();
+
+    strictEqual(actual, false);
+  });
+});
